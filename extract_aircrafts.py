063ac1251105e9a1b88301681ac498d382d26ebc
briocheausucre <maximecollignon@yahoo.fr>
2024-10-01 21:26:28 +0200

    aircrafts

--- extract_aircrafts.py
+++ extract_aircrafts.py
@@ -3,3 +3,9 @@ import pandas as pd
 aircrafts = pd.read_csv('https://opensky-network.org/datasets/metadata/aircraftDatabase.csv')
 aircrafts = aircrafts[aircrafts['manufacturername'].isin(['Airbus', 'Boeing'])]
 print(aircrafts.head())
+
+
+### OR ###
+
+aircrafts = pd.read_csv('Data/aircrafts.csv', index_col=0)
+print(aircrafts.head())
